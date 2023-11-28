8decbaa515fc70c61502de54fa8ecbffeddeb9a6
Mikel Azqueta <azquetamikel@gmail.com>
2023-11-28 18:00:08 +0100

    fix errors persistencia.py

--- persistencia.py
+++ persistencia.py
@@ -1,4 +1,6 @@
+"""Def funcion para guardar fichero pedidos.txt"""
 def guardar_pedido(nombre, apellidos):
+    """Abrir, escribir y cerrar"""
     with open("pedidos.txt", "a", encoding="utf-8") as file:
         file.write(nombre + " " + apellidos + "\n")
         file.close()
