b52967065c0a78c3ec2561079329a84a21f1c734
Mikel Azqueta <azquetamikel@gmail.com>
2023-11-28 17:50:45 +0100

    Fix errors api.py pylint

--- api.py
+++ api.py
@@ -1,11 +1,19 @@
+"""Run test"""
 def obtener_primer_elemento(lista):
+    """Obtener Primer elemento de la lista"""
     return lista[0]
 
+
 def inserta_elemento_al_final(lista, elemento):
+    """Insertar elemento al final"""
     lista.append(elemento)
 
+
 def inserta_elemento_al_principio(lista, elemento):
+    """Insertar elemento al principio"""
     lista.insert(0, elemento)
 
+
 def borra_lista(lista):
+    """Eliminar lista"""
     lista.clear()
