16de986bce573a18ed7fad77e4200f3c15f2c5d2
Mikel Azqueta <azquetamikel@gmail.com>
2023-11-28 18:11:13 +0100

    fix errors test_api pylint

--- test_api.py
+++ test_api.py
@@ -1,22 +1,33 @@
-import api
+""" 
+Pruebas de api 
+"""
 import pytest
+import api
+
 
 def test_obtener_primer_elemento():
+    """ Prueba Obtener primer elemento """
     lista = ["master", "full", "stack"]
     elemento = api.obtener_primer_elemento(lista)
     assert elemento == "master"
 
 def test_inserta_elemento_al_final():
+    """ Prueba Insertar elemento al final """
     lista = ["master", "full", "stack"]
     api.inserta_elemento_al_final(lista, "hola")
     assert lista == ["master","full","stack","hola"]
 
 def test_inserta_elemento_al_principio():
+    """ Prueba Insertar elemento al principio """
     lista = ["master", "full", "stack"]
     api.inserta_elemento_al_principio(lista, "hola")
     assert lista == ["hola", "master", "full", "stack"]
 
 def test_borra_lista():
+    """ Prueba borrar lista """
     lista = ["master", "full", "stack"]
     api.borra_lista(lista)
-    assert lista == []
+    assert not lista
+
+if __name__ == "__main__":
+    pytest.main()
