bac603bfc865897bd9d185216125cc6eb2b164d8
Mikel Azqueta <azquetamikel@gmail.com>
2023-11-28 17:07:00 +0100

    New margins in test_persistencia

--- test_persistencia.py
+++ test_persistencia.py
@@ -7,10 +7,10 @@ def test_guardar_pedido():
  Prueba general
  """
  with open("pedidos.txt", "w+", encoding="utf-8") as file:
-persistencia.guardar_pedido("Pedro", "Gil de Diego")
-persistencia.guardar_pedido("Michael", "Jordan")
-firstline = file.readline()
-secondline = file.readline()
+    persistencia.guardar_pedido("Pedro", "Gil de Diego")
+    persistencia.guardar_pedido("Michael", "Jordan")
+    firstline = file.readline()
+    secondline = file.readline()
 file.close()
- assert firstline == "-Pedro Gil de Diego\n"
- assert secondline == "-Michael Jordan\n"
+    assert firstline == "-Pedro Gil de Diego\n"
+    assert secondline == "-Michael Jordan\n"
