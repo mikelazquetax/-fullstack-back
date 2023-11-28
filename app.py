cef43d3a1b1fa1a79a9d8c08d13999ef64f4b9d8
Mikel Azqueta <azquetamikel@gmail.com>
2023-11-28 17:38:13 +0100

    fix errors en app.py con pylint

--- app.py
+++ app.py
@@ -1,32 +1,41 @@
+"""
+Creacion de Pedidos
+"""
 from flask import Flask, request, redirect, render_template, url_for
 
 app = Flask(__name__)
 
 def prepara():
+    """Renderizacion de pantalla"""
     return render_template('prepara_pedido.html')
 
 @app.route("/pizza", methods=['POST'])
 def pizza():
-    nombreCliente = request.form.get("p1")
-    apellidoCliente = request.form.get("p2")
-    print(nombreCliente)
-    print(apellidoCliente)
-    guardar_pedido(nombreCliente, apellidoCliente)
-    return redirect(url_for('solicita_pedido', nombreCliente=nombreCliente, apellidoCliente=apellidoCliente))
+    """Crea la solicitud de pizza y redirige a la pantalla final"""
+    nombre_cliente = request.form.get("p1")
+    apellido_cliente = request.form.get("p2")
+    print(nombre_cliente)
+    print(apellido_cliente)
+    guardar_pedido(nombre_cliente, apellido_cliente)
+    return redirect(url_for('solicita_pedido',
+    nombreCliente=nombre_cliente,apellidoCliente=apellido_cliente))
 
-def guardar_pedido(nombreCliente, apellidoCliente):
+def guardar_pedido(nombre_cliente, apellido_cliente):
+    """Guarda el nombre del cliente en el archivo pedidosPizza.txt"""
     with open("pedidosPizza.txt", "a", encoding="utf-8") as file:
-        file.write(nombreCliente + " " + apellidoCliente + "\n")
-        file.close()  
+        file.write(nombre_cliente + " " + apellido_cliente + "\n")
+        file.close()
 
 @app.route('/solicita_pedido')
 def solicita_pedido():
+    """Mostrar pagina final"""
     # Obtener valores de la URL
-    nombreCliente = request.args.get('nombreCliente')
-    apellidoCliente = request.args.get('apellidoCliente')
+    nombre_cliente = request.args.get('nombreCliente')
+    apellido_cliente = request.args.get('apellidoCliente')
 
     # Renderizar la plantilla de resultados con los valores
-    return render_template('solicita_pedido.html', nombreCliente=nombreCliente, apellidoCliente=apellidoCliente)
+    return render_template('solicita_pedido.html',
+    nombreCliente=nombre_cliente,apellidoCliente=apellido_cliente)
 
 if __name__ == '__main__':
     app.run(debug=True)
